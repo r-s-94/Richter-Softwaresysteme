fb453bb66dbc887451a7fe22f62867795894cae6
r.s. <richter.s.94@web.de>
2024-12-31 09:19:50 +0100

    Add gh action

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,22 @@ import "./App.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Technologies from "./component/technologies/technologies";
 /*
-
-*/
-function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: "/",
-        element: <Roots />,
-      },
-      {
-        path: "technologies",
-        element: <Technologies />,
-      },
-    ],
+,
     {
       basename: "/",
     }
-  );
+*/
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Roots />,
+    },
+    {
+      path: "technologies",
+      element: <Technologies />,
+    },
+  ]);
 
   return (
     <div id="oben" className="app-div">
